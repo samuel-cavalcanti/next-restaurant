cb4688c163d6302fc565c1e99f513f14104286cb
samuel-cavalcanti <scavalcanti111@gmail.com>
2021-02-15 23:49:59 -0300

    plateName agora é uma propriedade do compomente da pagina que é gerado pelo static paths

--- pages/aguarde/[plateName].js
+++ pages/aguarde/[plateName].js
@@ -1,23 +1,43 @@
-import { useRouter } from 'next/router'
-import { Spinner } from 'react-bootstrap';
+import {Spinner} from 'react-bootstrap';
 import styles from '../../styles/Home.module.css';
 import React from 'react';
 import logo from '../../components/logo';
 
-export default function Wait() {
 
-    const router = useRouter()
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {
+                params:
+                    {
+                        plateName: 'Café'
+                    }
+            }
+        ],
+        fallback: false
+    }
+}
 
-    const { plateName } = router.query;
+export async function getStaticProps(contex) {
+    const plateName = contex.params.plateName
+
+    return {
+        props: {
+            plateName
+        }
+    }
+}
+
+export default function Wait(props) {
 
     return (
 
         <div className={styles.container}>
             <main className={styles.main}>
-                <h1 className={styles.title} style={{ margin: 50 }} >
-                    Aguarde enquanto o {plateName} está sendo preparado
-                    </h1>
-                <Spinner animation="border" />
+                <h1 className={styles.title} style={{margin: 50}}>
+                    Aguarde enquanto o {props.plateName} está sendo preparado
+                </h1>
+                <Spinner animation="border"/>
             </main>
             {logo}
         </div>
